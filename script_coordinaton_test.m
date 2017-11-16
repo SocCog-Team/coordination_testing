7b96e4a23a7af2a6bac8fe3383736146df5a0b87
Anton Unakafov <antonunakafov@hotmail.com>
2017-11-16 14:05:25 +0100

    Description of the compound coordination test added

--- script_coordinaton_test.m
+++ script_coordinaton_test.m
@@ -18,8 +18,18 @@ for iFile = 1:nFile
           check_independence(isOwnChoice(:,200:end), isBottomChoice(:,200:end));      
 end
 
-%coordinating pairs: 
-% human pairs 3 and 5 (humanData_pair03.mat, humanData_pair05.mat); 
-% artificial agents in edData_best0.mat', 
-%   'rlData_noncuriousVSnoncurious2.mat', 
-%   'rlData_noncuriousVScurios.mat
+% PAIRS OVERVIEW
+%   human pairs:
+% humanData_pair01 - no coordination, P1 follows bottom target, P2 - own target
+% humanData_pair02 - fair coordination by choosing bottom target
+% humanData_pair03 - not-quite-fair coordination (beneficial for P1) by turn-taking
+% humanData_pair04 - no coordination
+% humanData_pair05 - slightly unfair coordination (beneficial for P2) by turn-taking
+%   artificial agents with memory 1 using evolutionary evolved turn-taking:
+% edData_best0 (agents using evolutionary evolved turn-taking) - perfect coordination
+%   Reinforcement-learning-based agents with various exploration levels
+% rlData_noncuriousVSnoncurious2 - fair coordination by choosing bottom target
+% rlData_noncuriousVScurios - unfair coordination: P1 follows own target, 
+%    but sometimes SHORTLY switches to turn-taking; P2 follows P1.
+% rlData_curiosVScurios.mat - both Players prefer own target and explore
+%    too much to establish stable coordination
